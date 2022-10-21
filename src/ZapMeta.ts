0eb696457bc84a8b4c77ce65d17dc93ec3477efe
Mark Aiken <gwmaiken@att.net>
2022-10-20 18:10:51 -0700

    add zapper configs

---
+++ src/ZapMeta.ts
@@ -0,0 +1,30 @@
+import { BigNumber, Signer } from 'ethers'
+import {Token} from "./entities";
+
+export interface ScalingDetails {
+    scaleVia: 'mul' | 'div'
+    scalingFactor: number
+}
+
+export interface ScalingInfo {
+    priceScalingFactor?: ScalingDetails
+    liquidationScalingFactor?: ScalingDetails
+}
+
+export default interface ZapMeta {
+    vaultType: 'YEARN' | 'BEEFY'
+    underlying: Token
+    underlyingPriceSourceAddress: string
+    zapperAddress: string
+    zapInFunction: (amount: BigNumber, vaultIndex: BigNumber, signer: Signer) => any
+    zapOutFunction: (amount: BigNumber, vaultIndex: BigNumber, signer: Signer) => any
+    collateralScaling?: ScalingInfo
+    underlyingScaling?: ScalingInfo
+}
+
+export interface CamMeta {
+    underlying: Token
+    underlyingPriceSourceAddress: string
+    amTokenAddress: string
+    camTokenAddress: string
+}
