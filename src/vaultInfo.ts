9bf9d1b9750ebf5dd85d3b44c9a8d1df0f92ce56
Mark Aiken <2439803+royalaid@users.noreply.github.com>
2023-02-07 17:11:44 -0800

    add fallback and token descriptions for gDAI

--- src/vaultInfo.ts
+++ src/vaultInfo.ts
@@ -1754,7 +1754,8 @@ const MATIC_COLLATERALS = [
     vaultAddress: GDAI_VAULT_ADDRESS,
     contractAbi: StableQiVault__factory.abi,
     version: 2,
-    underlyingIds: ['daidai'],
+    fallbackUnderlyingAddress: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',
+    underlyingIds: ['gns', 'daidai'],
   },
   {
     shortName: 'maticx',
